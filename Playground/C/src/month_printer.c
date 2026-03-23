dc9da296e9b49df1a6afde11f9c7ac154fb88b07
Suika-chan86 <Ljqbwlb2435@outlook.com>
2026-03-23 22:34:04 +0800

    feat: 增强代码鲁棒性

--- Playground/C/src/month_printer.c
+++ Playground/C/src/month_printer.c
@@ -3,10 +3,20 @@
 int main(void)
 {
     char *m[] = {"January\n","February\n","March\n","April\n","May\n","June\n","July\n","August\n","September\n","October\n","November\n","December\n",};
+    
     int i = 0;
     
     printf("请输入月份：");
-    scanf("%d", &i);
+    // 检查输入是否为数字
+    if(scanf("%d", &i) != 1) {
+        printf("请输入数字！\n");
+        return 1;
+    }
 
-    printf("%s", m[i-1]);
+    if (i >= 1 && i <= 12) {
+        printf("%s", m[i-1]);
+    } else {
+        printf("请输入1~12的整数！\n");
+    }
+        
 }
